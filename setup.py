5c946a1124630df9bf50aefc423a0d29b7f55ce6
qoda <jpbydendyk@gmail.com>
2017-11-14 17:10:24 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jmbo-search',
-    version='0.0.1',
+    version='0.1.0',
     description='Jmbo search application.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Consulting',
